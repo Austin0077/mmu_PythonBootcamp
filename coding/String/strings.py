5119c5bd93b192100dc19dc968b80abdbc51cadc
kafomambia <kafomambia@gmail.com>
2017-04-16 08:45:40 +0300

    first string commit

---
+++ coding/String/strings.py
@@ -0,0 +1,18 @@
+# string literals
+print('\n'*2)
+print('Raw string: place befofr a quotationa mark to make a string a raw string')
+print('Raw string completely ignores any escape charater and prints any escape character in a string:')
+print('\n'*2)
+
+print('my sister\'s kid is 21 years old')
+print('\n'*2)
+
+print('Multi_line strings using the triple quotes')
+print('\n')
+
+print(''' Dear sir/madam \n 
+You are highly welcome to the mmu graduation day.
+it will be held with the university prmises.\n
+yours sincerely,
+Omambia Programmer.''' )
+
